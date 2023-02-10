22ea72916929382823cc635135b17becc2c448f3
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-10 17:27:46 +0530

    added switch case

--- src/com/bl/Employeewage/EmployeeWage.java
+++ src/com/bl/Employeewage/EmployeeWage.java
@@ -2,19 +2,33 @@ package com.bl.Employeewage;
 
 public class EmployeeWage
 {
+    public static final int isPartTime = 2;
+    public static final int isFullTime = 1;
+    public static final int empRatePerHour = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage program");
-        int isPartTime =2;
-        int isFullTime = 1;
-        int empRatePerHour = 20;
         int empHrs = 0;
         int totalWage = 0;
+
         double empcheck= Math.floor(Math.random() *10) % 3;
-        if (empcheck == isFullTime)
-            empHrs = 8;
-        else if (empcheck == isPartTime)
-            empHrs = 4;
+
+        switch ((int)empcheck){
+
+            case isFullTime:
+                empHrs=8;
+                break;
+
+            case isPartTime:
+                empHrs=4;
+                break;
+
+            default:
+                empHrs=0;
+
+        }
+
         totalWage = empHrs * empRatePerHour;
+
         System.out.println("Emp WAge: " + totalWage);
     }
 }
